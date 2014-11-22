7df89a2b2cf2048ab591b1e0234ab160414f256f
Mason McGill <mason.b.mcgill@gmail.com>
2014-11-21 20:34:56 -0800

    Added PyPI files.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='fauxton',
+      packages=['fauxton'],
+      version='0.1',
+      description=('Computer graphics technology for computer vision'
+                   ' applications.'),
+      author='Mason McGill',
+      author_email='mmcgill@caltech.edu',
+      url='https://github.com/MasonMcGill/fauxton',
+      download_url='https://github.com/MasonMcGill/fauxton/tarball/0.1',
+      keywords=['vision', 'graphics', 'blender'],
+      classifiers=[])
